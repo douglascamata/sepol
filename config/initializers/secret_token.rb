e27ad3e0b677da29f74748ffb14fa4066f7ac25c
Thawan Fidelis <thawan.fidelis@gmail.com>
2012-09-08 17:19:51 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sepol::Application.config.secret_token = '564b9e7784fe7a8ccc8017fa86d1a256db9b60b299a92e6619674d8927a632404bef8c1791cdd0eb1995ee7f38b3c0af8dc061440454b9ace3f4acb62ec54185'
