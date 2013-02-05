bf9873f282bf4b16e8ea83c20b47ac480ebbca6b
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-02-05 20:06:04 -0200

    Adicionando teste para login de administrador

--- spec/factories/administradores.rb
+++ spec/factories/administradores.rb
@@ -2,5 +2,9 @@
 
 FactoryGirl.define do
   factory :administrador do
+  	sequence(:nome) {|n| "usuario_%s" % n }
+  	sequence(:email) {|n| "usuario%s@gmail.com" % n }
+    password '12345678'
+    password_confirmation '12345678'
   end
 end
