02614c9151c8b77a4ffafbd9a61aab7117964870
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-02-05 18:53:51 -0200

    Adicionando campo NOME nas views de administrador

--- app/models/administrador.rb
+++ app/models/administrador.rb
@@ -6,6 +6,6 @@ class Administrador < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :nome
   # attr_accessible :title, :body
 end
