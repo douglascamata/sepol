4a5063c07dbf2f092b655453208a5b1fdb6feb94
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-02-16 21:54:19 -0200

    Adicionando avatar aos usuarios

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,10 +2,13 @@ class Usuario < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :nome, :cargo, :lattes
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :nome, :cargo, :lattes, :avatar
   # attr_accessible :title, :body
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130207211405) do
+ActiveRecord::Schema.define(:version => 20130216233755) do
 
   create_table "administradores", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -55,6 +55,10 @@ ActiveRecord::Schema.define(:version => 20130207211405) do
     t.string   "cargo"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "usuarios", ["email"], :name => "index_usuarios_on_email", :unique => true
