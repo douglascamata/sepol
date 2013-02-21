c79fa3740d0519175eaf088aa4081ab4a2e9a3a1
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-02-21 18:15:46 -0300

    Adicionando os arquivos de testes para publicacoes

---
+++ spec/factories/publicacoes.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :publicacao do
+    titulo "MyString"
+    abstract "MyText"
+    link "MyText"
+  end
+end
