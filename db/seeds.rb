2a596c433400551adfb44738752b8d938e4778f7
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-02-27 21:44:41 -0300

    Adicionando seed para publicacoes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Publicacao.create!(titulo: "Photothermal characterization of low density polyethylene food packages",
+				   abstract: "The present work discuss the applicability of photothermal techniques for determining diffusion coefficients of 
+							   oxygen and carbon dioxide of commercial low-density polyethylene (LDPE). The methodology involves the monitoring 
+							   of diffused gas by a photoacoustic analyzer. Diffusion coefficients measured for CO2 and O2 were 2.77 x 10-8 cm2/s 
+							   and 1.68 x 10-7 cm2/s, respectively. To support the gas diffusion results, thermal properties were studied using 
+							   photoacoustic spectroscopy and crystallinity was determined using X-ray diffraction. Values obtained for thermal 
+							   diffusivity and specific heat capacity were 1.65 x 10-3cm2/s and 2.33 J.cm-3K-1, which are in good agreement with 
+							   values available in the literature for pure LDPE and thus assure reliability of diffusion coefficients values.",
+					
+					link: "http://www.scielo.br/scielo.php?script=sci_pdf&pid=S0104-14282004000100007&lng=en&nrm=iso&tlng=en"
+					)
+Publicacao.create!(titulo: "Effects of hydroxyvalerate contents in thermal degradation kinetic of cellulose acetate propionate/poly(3-hydroxyalkanoates) blends", 
+				   abstract: "Thermal stability of polymers is an important parameter that determines the application as well as the processing 
+				   			  conditions. The green polymers have shown low thermal stability, such as the polyhydroxyalkanoates (PHAs). The PHAs 
+				   			  with different comonomers containing hydroxyvalerate (HV) were studied. It was seen that the green polymer showed a 
+				   			  fast thermal degradation process. The addition of the HV comonomer modified this profile and the thermal degradation 
+				   			  kinetic. The blend prepared between the PHAs and other polymers can modify the thermal degradation process of the 
+				   			  green polymers. In the present study, blends of cellulose acetate propionate and PHAs were prepared, and the thermal 
+				   			  degradation kinetics of these blends were evaluated. It was observed that the cellulose acetate propionate (CAP) 
+				   			  phase in the blends modified the thermal degradation process and kinetic profile of the PHA phase. In the blends, 
+				   			  the thermal stability of the PHAs was slightly modified because of CAP reducing the reactivity of the PHAs. On the 
+				   			  other hand, the thermal stability of the CAP phase in the blends is not largely modified by the PHA phase. However, 
+				   			  the hydroxyvalerate comonomer decreases the reactivity of the CAP phase at the start of thermal degradation of the 
+				   			  same. The interaction between the phases promotes the synergetic interaction, which slightly improves the thermal 
+				   			  stability of the two polymers blends." , 
+				   link: "http://link.springer.com/content/pdf/10.1007%2Fs10973-011-2152-y" 
+				  )
+
+Publicacao.create!(titulo: "Mechanical Properties and Abrasive Behavior of Diamond Incorporated Epoxy Composites",
+				   abstract: "The thermal and mechanical properties assessed respectively by dynamic mechanical analysis and flexural bend tests as well as the abrasive behavior obtained by wear tests of diamond particles incorporated epoxy matrix composites were investigated. Diamond particles with sizes in the range of 45 to 115 µm, synthesized at high pressure and temperature, were mixed in amounts of 20 and 40 wt% with dyglycidyl of the bisphenol A, DGEBA, epoxy resin cured with stoichiometric ratio of tetraetylenepentamine, TEPA, hardener. These composites were dynamic mechanical, DMA analyzed and three points bend tested. The behavior of the composites as abrasive tool for industrial polishing of ornamental rocks was evaluated by wear tests. The results showed an improved performance of the DGEBA/TEPA composites with incorporation of diamond particles.",
+				   link: "http://www.scientific.net/MSF.727-728.1757")
+
+Publicacao.create!(titulo: "Thermal degradation behavior of a carboxylic acid-terminated amphiphilic block copolymer poly(ethylene)-b-poly(ethylene oxide)" ,
+				   abstract: "The thermal degradation of an amphiphilic block copolymer poly(ethylene)-b-poly(ethylene oxide)-carboxylic acid terminated (PE-b-80%PEO–CH2COOH) and its salt obtained as intermediary product from chemical oxidation of the end group of poly(ethylene)-b-poly(ethylene oxide) (PE-b-80%PEO) has been studied using a thermogravimetric mass spectrometry (TG/MS) coupled system. The isothermal fragmentation of PE-b-80%PEO–CH2COOH showed a more complex fragmentation pattern than PE-b-80%PEO owing to the simultaneous occurrence of the polyether block and the carboxylic end group fragmentations. This led to the appearance of four overlapping ion current peaks of fragments with m/z 44 and two peaks relative to m/z 18 at different times by acid-terminated copolymer. For the PE-b-80%PEO copolymer, two ion current peaks associated to m/z 44 and one large peak relative to m/z 18 fragments were detected. The intermediary product (PE-b-80%PEO–CH2COO− K+) showed differences related to the fragmentation behavior. It has more defined ion current signals and presented characteristic peaks attributed to m/z 43 fragment at the very beginning of the thermal degradation process, which it not detected in the acid copolymer.",
+				   link: "http://link.springer.com/content/pdf/10.1007%2Fs10973-010-1094-0" )
+
+Publicacao.create!(titulo: "Characterization and Properties of Hydrophilic Cellulose Acetate Propionate Derivative" ,
+				   abstract: "The chemical modification of Acrylamidomethyl Cellulose Acetate Propionate (AMCAP) was carried out by radical addition of acrylic acid. The structural modification was confirmed with the aid of FTIR, MS and NMR techniques. Thermal properties of hydrophilic cellulose derivative (AMCAP–H2O2) such as glass transition (Tg 153 °C) and thermal stability (372.7 °C) were determined by DSC and TGA techniques, respectively. These thermal properties confirmed the introduction of carboxylic groups into AMCAP structure, which causes an impact in their properties. The AMCAP–H2O2 shows minor contact angle compared to AMCAP, giving a more hydrophilic characteristic, due to acrylic acid addition into the side chains of AMCAP polymer.",
+				   link: "http://link.springer.com/content/pdf/10.1007%2Fs10924-010-0221-0" )
+
+Publicacao.create!(titulo: "Effect of the lithium cation on the sorption and electrical properties of amphiphilic block copolymers and their composites" ,
+				   abstract: "The use of lithium cation in composites of block copolymers [polyethylene-b-polyethylene oxide (PE-b-50%PEO and PE-b-80%PEO)] and their derivatives was tested as a modifier of the vapor sorption and impedance of these complexes. The block copolymer PE-b-80%PEO was modified by oxidation of its hydroxyl end group to both a carboxylic acid group (PE-b-80%PEO)CH2COOH and its sodium salt (PE-b-80%PEO)CH2COO− Na+ for the purpose of improving its compatibility and performance as a matrix for composites. These modified copolymers were characterized by FTIR, DSC, and mass spectrometry. The sorption of water of these copolymers and their composites with lithium nitrate was also compared, as well as the electrical properties of their composites were measured by electrical impedance spectroscopy. For the composites obtained with PE-b-80%PEO and lithium nitrate, it was found that lithium cation plays an important role increasing the sorption rate, which is maximized for the PE-b-80%PEO + (21% lithium nitrate) composite. For the copolymers (PE-b-80%PEO)CH2COOH and (PE-b-80%PEO)CH2COO− Na+ and their composites, the highest sorption rate was observed for salt in the following order: COO− Na+ > COOH > OH. The PE-b-80%PEO + (21% lithium nitrate) composite behaves as a solid polymeric ionic conductor fitting the Williams–Landel–Ferry equation. However, both (PE-b-80% PEO)CH2COOH and (PE-b-80%PEO)CH2COO− Na+ + (21% lithium nitrate) composites fitted the Variable Range Hopping equation, indicating a conductance trend with temperature governed by a thermally activated with energy of 0.482 and 0.524 eV and not by a relaxation process. © 2010 Wiley Periodicals, Inc. J Polym Sci Part B: Polym Phys 48: 1809–1817, 2010" ,
+				   link: "http://onlinelibrary.wiley.com/doi/10.1002/polb.22048/pdf" )
