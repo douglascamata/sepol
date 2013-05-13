8bc3b68cbdfdc1407acdfb71f959946415ef65fa
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-05-13 20:18:33 -0300

    Adicionando testes para equipamentos

--- app/models/equipamento.rb
+++ app/models/equipamento.rb
@@ -1,6 +1,8 @@
 class Equipamento < ActiveRecord::Base
   has_attached_file :imagem, :styles => { :medium => "800x600>", :thumb => "200x200#" }		
   
+  validates_presence_of :nome, :descricao, :imagem
+
   attr_accessible :descricao, :nome, :imagem
   self.per_page = 12
 end
--- spec/models/equipamento_spec.rb
+++ spec/models/equipamento_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Equipamento do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Nome' do
+  	it{should_not have_valid(:nome).when('', nil)}
+  	it{should have_valid(:nome).when("Estufa")}
+  end
+  context 'Descricao' do
+  	it{should_not have_valid(:descricao).when('', nil)}
+  	it{should have_valid(:descricao).when("Lorem ipsum dolor sit amet, consectetuer")}
+  end
+  context 'Imagem' do
+  	it{should_not have_valid(:imagem).when(nil)}
+  end
 end
