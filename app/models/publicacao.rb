5fca7641b56c42f6a63edd3bcda3b51ff5205270
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-05-13 20:19:14 -0300

    Adicionando testes de unidade para publicacao

--- app/models/publicacao.rb
+++ app/models/publicacao.rb
@@ -1,3 +1,4 @@
 class Publicacao < ActiveRecord::Base
   attr_accessible :abstract, :link, :titulo
+  validates_presence_of :abstract, :titulo
 end
--- spec/models/publicacao_spec.rb
+++ spec/models/publicacao_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Publicacao do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context 'Titulo' do
+		it{should_not have_valid(:titulo).when('', nil)}
+		it{should have_valid(:titulo).when("Titulo qualquer")}
+	end
+	context 'Abstract' do
+		it{should_not have_valid(:abstract).when('', nil)}
+		it{should have_valid(:abstract).when("Abstract qualquer")}
+	end
 end
