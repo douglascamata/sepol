644e901ebe2ba99a906f27a9761ca7d9daee5ab7
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-05-13 21:16:48 -0300

    mudando alguns testes

--- spec/acceptance/usuario_spec.rb
+++ spec/acceptance/usuario_spec.rb
@@ -3,28 +3,36 @@
 require 'spec_helper'
 
 feature 'Login' do
+	scenario 'Criar usuario' do				
+		visit new_usuario_registration_path
+		fill_in 'Email', with: 'usuario@test.br'
+		fill_in 'Password', with: '123456foobar'
+		fill_in 'Password confirmation', with: '123456foobar'
+		fill_in 'Nome', with:'usuario'
+		fill_in 'Lattes', with: 'http://buscatextual.cnpq.br/usuario'
+		fill_in 'Cargo', with: 'bolsita'
+		click_button 'Sign up'
+		page.should have_content "Login efetuado com sucesso."				
+	end
+
 	scenario 'fazer login' do	
 		usuario = FactoryGirl.create :usuario, nome: 'usuario', email: 'usuario@test.br', password: '123456foobar', password_confirmation: '123456foobar', lattes: 'http://buscatextual.cnpq.br/usuario', cargo: 'bolsita'
-		visit new_usuario_registration_path
+		visit new_usuario_session_path
 		fill_in 'Email', with: usuario.email
 		fill_in 'Password', with: usuario.password
-		fill_in 'Nome', with: usuario.nome
-		fill_in 'Lattes', with: usuario.lattes
-		fill_in 'Cargo', with: usuario.cargo
-		click_button 'Sign up'		
+		click_button 'Sign in'	
+		page.should have_content "Login efetuado com sucesso!"		
 	end
 end
+
 
 feature 'Listar Usuarios' do
 	scenario 'Listar todos usuarios' do
 		usuario = FactoryGirl.create :usuario
-
 		visit usuarios_path
-		# -usuarios.each do |usuario|
-			page.should have_content usuario.nome
-			page.should have_content usuario.email
-			page.should have_content usuario.lattes
-			page.should have_content usuario.cargo
-		# end
+		page.should have_content usuario.nome
+		page.should have_link('Email', href: "mailto:#{usuario.email}")
+		page.should have_link('Lattes', href: usuario.lattes)
+		page.should have_content usuario.cargo	
 	end
 end	
