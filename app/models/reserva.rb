8fd7076ff5b4005955b2243fa9ff76f05ec65156
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-05-20 18:51:24 -0300

    Implementacao da validacao de horarios da reserva

--- app/models/reserva.rb
+++ app/models/reserva.rb
@@ -3,14 +3,13 @@ class Reserva < ActiveRecord::Base
   belongs_to :equipamento
   attr_accessible :horario_final, :horario_inicial, :usuario_id, :equipamento_id
   validates_presence_of :usuario_id, :equipamento_id, :horario_inicial, :horario_final
-	before_save :verificar_disponibilidade_de_horario
+	before_save :validar_horario
 end
 
-def verificar_disponibilidade_de_horario
-	@horarios_iniciais = []
-	@horarios_finais = []
+def validar_horario
+	raise ArgumentError if self.horario_final < self.horario_inicial
+	@horarios_iniciais, @horarios_finais = [], []
 	Reserva.all.each{|reserva| @horarios_iniciais << reserva.horario_inicial; @horarios_finais << reserva.horario_final}
-	
 	@horarios = @horarios_iniciais.zip @horarios_finais	
-	@horarios.each{|inicio, fim| raise ArgumentError if self.horario_inicial.between? inicio, fim}
+	@horarios.each{|inicio, fim| raise ArgumentError if (self.horario_inicial.between?(inicio, fim) or self.horario_final.between?(inicio, fim))}
 end
