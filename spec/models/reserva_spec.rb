0266e50a942b9f2102af3515f9200c7140790b13
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-05-28 03:36:17 -0300

    Novo caso para horarios e retirando o delorean dos testes

--- spec/models/reserva_spec.rb
+++ spec/models/reserva_spec.rb
@@ -28,12 +28,12 @@ describe Reserva do
     it 'Horario final nao pode estar entre horarios ja existentes' do
       equipamento = FactoryGirl.create :equipamento
       FactoryGirl.create :reserva, equipamento: equipamento
-      lambda{FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: time_travel_to(1.days.ago){Delorean.now}, horario_final: time_travel_to(30.minutes.from_now){Delorean.now}}.should raise_exception
+      lambda{FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: Time.now.yesterday, horario_final: Time.now + 30.minutes}.should raise_exception
     end
 
     it  'Uma reserva nao pode acabar antes de começar' do
       equipamento = FactoryGirl.create :equipamento
-      lambda{FactoryGirl.create :reserva, equipamento: equipamento, horario_final: time_travel_to(1.day.ago){Delorean.now}}.should raise_exception
+      lambda{FactoryGirl.create :reserva, equipamento: equipamento, horario_final: Time.now.yesterday}.should raise_exception
     end
 
     it 'Horario final nao pode ser igual a horario final' do
@@ -41,14 +41,23 @@ describe Reserva do
       lambda{FactoryGirl.create :reserva, equipamento: equipamento, horario_final: horario, horario_final: horario}.should raise_exception
     end
 
+    it 'Deve verificar se nao existe uma reserva no intervalo de tempo solicitado' do
+      horario = Time.now
+      equipamento = FactoryGirl.create :equipamento
+      FactoryGirl.create :reserva, equipamento: equipamento,  horario_inicial: horario, horario_final: horario + 1.hour
+      lambda{FactoryGirl.create :reserva, equipamento: equipamento,  horario_inicial: horario - 1.hour, horario_final: horario + 2.hours}.should raise_error
+    end
+
+
     it 'Um equipamento pode ter varias reservas' do
       equipamento = FactoryGirl.create :equipamento
+      horario = Time.now
       reserva_1 = FactoryGirl.create :reserva, equipamento: equipamento
-      reserva_2 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: time_travel_to(2.days.ago){Delorean.now}
-      reserva_3 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: time_travel_to(3.days.ago){Delorean.now}
-      reserva_4 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: time_travel_to(4.days.ago){Delorean.now}
-      reserva_5 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: time_travel_to(5.days.ago){Delorean.now}
-      reserva_6 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: time_travel_to(6.days.ago){Delorean.now}
+      reserva_2 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: horario + 2.days, horario_final: horario + 2.days + 2.hours
+      reserva_3 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: horario + 3.days, horario_final: horario + 3.days + 2.hours
+      reserva_4 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: horario + 4.days, horario_final: horario + 4.days + 2.hours
+      reserva_5 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: horario + 5.days, horario_final: horario + 5.days + 2.hours
+      reserva_6 = FactoryGirl.create :reserva, equipamento: equipamento, horario_inicial: horario + 6.days, horario_final: horario + 6.days + 2.hours
       equipamento.reservas.should include(reserva_1, reserva_2, reserva_3, reserva_4, reserva_5, reserva_6)
     end
   end
