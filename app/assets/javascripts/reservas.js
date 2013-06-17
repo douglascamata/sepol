3793a8426981bed3ec4685406ef8ac63f735ec9e
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-06-16 21:20:39 -0300

    Adicionando javascript de datepicker

---
+++ app/assets/javascripts/reservas.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('#datetimepicker_inicio').datetimepicker({
+		    format: 'dd/MM/yyyy hh:mm:ss',
+  			language: 'pt-BR'
+	});
+  $('#datetimepicker_fim').datetimepicker({
+        format: 'dd/MM/yyyy hh:mm:ss',
+        language: 'pt-BR'
+  });
+});
