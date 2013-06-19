f0f46aef6d86491e8c4aaeb8a5c610d9d8692492
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-06-19 01:34:08 -0300

    Adicionando a relacao de resultado e reserva

--- app/models/resultado.rb
+++ app/models/resultado.rb
@@ -3,5 +3,6 @@ class Resultado < ActiveRecord::Base
   attr_accessible :imagem
   has_attached_file :imagem, :styles => { :medium => "800x600>", :thumb => "200x200#" }
 
+	attr_accessible :reserva_id
   validates_presence_of :reserva_id
 end
