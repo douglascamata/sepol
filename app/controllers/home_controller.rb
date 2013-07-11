422948d933154601579bf552f2773261684578e2
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-07-11 04:43:09 -0300

    Adicionando autorização padrao para home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 #encoding: utf-8
 class HomeController < ApplicationController
+	load_and_authorize_resource
   def index
   	@home = Home.new titulo: 'Sepol', conteudo: 'Bem vindo a Pagina do Setor de Polímeros da UENF'
   end
