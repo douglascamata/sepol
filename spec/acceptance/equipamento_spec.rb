cbd68960410dadb7435a71d90076919e7b2ad781
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-07-11 04:51:09 -0300

    Adicionando teste de aceitação para equipamentos e publicações

---
+++ spec/acceptance/equipamento_spec.rb
@@ -0,0 +1,50 @@
+#encoding: utf-8
+
+require 'spec_helper'
+
+feature 'Adicionar equipamento' do
+  scenario 'Não pode ser realizado por visitante' do
+      visit new_equipamento_path
+      page.should have_content 'Você precisa estar logado para acessar essa página.'    
+  end
+
+  scenario 'Não pode ser realizado por qualquer usuario' do
+    usuario = FactoryGirl.create :usuario
+    logar(usuario)
+    visit new_equipamento_path
+    page.should have_content 'Você precisa logar como administrador para acessar essa página.'    
+  end
+
+
+  scenario 'Com sucesso' do 
+    usuario = FactoryGirl.create :usuario, admin: true
+    logar(usuario)
+
+    visit new_equipamento_path
+    fill_in 'Nome', with: 'Super Equipamento'
+    fill_in 'Descrição', with: 'Alguma Descrição'
+    attach_file('Imagem', 'app/assets/images/missing.jpg')
+    click_button 'Save'
+    page.should have_content 'Equipamento criado com sucesso!'
+  end
+
+  scenario 'Sem sucesso' do
+    usuario = FactoryGirl.create :usuario, admin: true
+    logar(usuario)
+    visit new_equipamento_path
+    fill_in 'Nome', with: ''
+    fill_in 'Descrição', with: ''
+    click_button 'Save'
+    page.should have_content 'Nome não pode ficar em branco'
+    page.should have_content 'Descrição não pode ficar em branco'
+    page.should have_content 'Imagem não pode ficar em branco'
+  end
+end
+
+feature 'Listar Equipamentos' do
+  scenario 'Listar todos equipamentos' do
+    10.times{FactoryGirl.create :equipamento}
+    visit equipamentos_path
+    Equipamento.all.each{|equipamento| page.should have_content equipamento.nome}
+  end
+end
--- spec/acceptance/reserva_spec.rb
+++ spec/acceptance/reserva_spec.rb
@@ -1,54 +1,90 @@
 #encoding: utf-8
 
 require 'spec_helper'
-require './spec/suport/usuario.rb'
 
 feature 'Reserva de equipamento' do
-	scenario 'Efetuar reserva de equipamento' do
-		usuario = FactoryGirl.create :usuario
-		equipamento = FactoryGirl.create :equipamento
-		horario_inicio  = Time.now.utc
-		horario_final = horario_inicio + 2.hours
-		logar(usuario)
-		visit new_equipamento_reserva_path(equipamento)
-		fill_in "Horario inicial", with: horario_inicio
-		fill_in "Horario final", with: horario_final
-		click_button 'Save'
+  scenario 'Apenas usuarios logados podem acessar essa pagina.' do
+    equipamento = FactoryGirl.create :equipamento
+    visit new_equipamento_reserva_path(equipamento)
+    page.should have_content 'Você precisa estar logado para acessar essa página.'
+  end
 
-		visit equipamento_reservas_path(equipamento.id)
-		page.should have_content horario_inicio.strftime("%T - %D")
-		page.should have_content horario_final.strftime("%T - %D")
-	end
+  scenario 'Efetuar reserva de equipamento' do
+    usuario = FactoryGirl.create :usuario
+    equipamento = FactoryGirl.create :equipamento
+    horario_inicio  = Time.now.utc
+    horario_final = horario_inicio + 2.hours
+    logar(usuario)
+
+    visit new_equipamento_reserva_path(equipamento)
+    
+    fill_in "Horario inicial", with: horario_inicio
+    fill_in "Horario final", with: horario_final
+    click_button 'Save'
+
+    page.should have_content "Reserva feita com sucesso."
+  end
 end
 
 feature 'Comentario de reserva' do
-	scenario 'Deve ser possivel fazer comentarios sobre a reserva' do
-		equipamento = FactoryGirl.create :equipamento
-		reserva = FactoryGirl.create :reserva, equipamento: equipamento
-		usuario = FactoryGirl.create :usuario
-		logar(usuario)
+  scenario 'Apenas usuarios logados podem acessar essa pagina.' do
+    equipamento = FactoryGirl.create :equipamento
+    reserva = FactoryGirl.create :reserva, equipamento: equipamento
+    visit equipamento_reserva_comentarios_path(equipamento, reserva)
+    page.should have_content 'Você precisa estar logado para acessar essa página.'
+  end
 
-		visit equipamento_reserva_comentarios_path(equipamento, reserva)
+  scenario 'Deve ser possivel fazer comentarios sobre a reserva' do
+    usuario = FactoryGirl.create :usuario
+    equipamento = FactoryGirl.create :equipamento
+    reserva = FactoryGirl.create :reserva, equipamento: equipamento, usuario: usuario
+    logar(usuario)
 
-		fill_in "comentario", with: "Aqui esta um comentario."
-		click_button "Enviar"
+    visit equipamento_reserva_comentarios_path(equipamento, reserva)
+    find("#lB").click
 
-		page.should have_content "Aqui esta um comentario."
-	end
+    fill_in "comentario", with: "Aqui esta um comentario."
+    click_button "Enviar"
+
+    page.should have_content "Aqui esta um comentario."
+  end
 end
 
-feature	'Informações da reserva' do
-	scenario 'Deve conter as informacoes basicas da reserva' do
-		equipamento = FactoryGirl.create :equipamento
-		reserva = FactoryGirl.create :reserva, equipamento: equipamento
-		usuario = FactoryGirl.create :usuario
-		logar(usuario)
+feature 'Informações da reserva' do
+  scenario 'Apenas usuarios logados podem acessar essa pagina.' do
+    equipamento = FactoryGirl.create :equipamento
+    reserva = FactoryGirl.create :reserva, equipamento: equipamento
+    visit equipamento_reserva_comentarios_path(equipamento, reserva)
+    page.should have_content 'Você precisa estar logado para acessar essa página.'
+  end
 
-		visit equipamento_reserva_comentarios_path(equipamento, reserva)		
+  scenario 'Deve conter as informacoes basicas da reserva' do
+    equipamento = FactoryGirl.create :equipamento
+    reserva = FactoryGirl.create :reserva, equipamento: equipamento
+    usuario = FactoryGirl.create :usuario
+    logar(usuario)
 
-		page.should have_content usuario.nome
-		page.should have_content reserva.horario_inicial.strftime("%D - %T")
-		page.should have_content reserva.horario_final.strftime("%D - %T")
-		page.should have_content equipamento.nome
-	end
+    visit equipamento_reserva_comentarios_path(equipamento, reserva)    
+
+    page.should have_content usuario.nome
+    page.should have_content reserva.horario_inicial.strftime("%D - %T")
+    page.should have_content reserva.horario_final.strftime("%D - %T")
+    page.should have_content equipamento.nome
+  end
+
+  scenario 'O Usuario só pode ter acesso as informações das suas reservas' do
+    equipamento_1 = FactoryGirl.create :equipamento, nome: 'Equipamento 1'
+    equipamento_2 = FactoryGirl.create :equipamento, nome: 'Equipamento 2'
+    usuario_1 = FactoryGirl.create :usuario
+    usuario_2 = FactoryGirl.create :usuario
+    FactoryGirl.create :reserva, equipamento: equipamento_2, usuario: usuario_2
+    FactoryGirl.create :reserva, equipamento: equipamento_1, usuario: usuario_1
+    
+    logar(usuario_1)
+    visit reservas_path
+    
+    page.should have_content equipamento_1.nome
+    page.should_not have_content equipamento_2.nome
+  end
+
 end
