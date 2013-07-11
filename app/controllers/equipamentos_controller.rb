af033de45ab1c4ff2435cec165287d1be9a56b0f
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-07-11 04:42:35 -0300

    Adicionando msg flash de criacao de equipamento

--- app/controllers/equipamentos_controller.rb
+++ app/controllers/equipamentos_controller.rb
@@ -1,5 +1,10 @@
 class EquipamentosController < InheritedResources::Base
+	load_and_authorize_resource
 	def index
 		@equipamentos = Equipamento.paginate(:page => params[:page])
+	end
+
+	def create
+		create! notice: 'Equipamento criado com sucesso!'
 	end
 end
