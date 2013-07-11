685875d0d7b8b3b89a562dc71f76f1dbb0c871be
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-07-11 04:45:36 -0300

    Adicionando autorização basica para comentarios

--- app/controllers/resultados_controller.rb
+++ app/controllers/resultados_controller.rb
@@ -1,14 +1,15 @@
 class ResultadosController < InheritedResources::Base
-	def new
-		@equipamento = Equipamento.find(params[:equipamento_id])
-		@reserva = Reserva.find(params[:reserva_id])
-		@resultado = Resultado.new
-	end
+  load_and_authorize_resource
+  def new
+    @equipamento = Equipamento.find(params[:equipamento_id])
+    @reserva = Reserva.find(params[:reserva_id])
+    @resultado = Resultado.new
+  end
 
-	def create
-		@resultado = Resultado.new(params[:resultado])
-		@resultado.reserva_id = params[:reserva_id]
-		@resultado.save!
-		redirect_to equipamento_reserva_path(params[:equipamento_id], params[:reserva_id])
-	end
+  def create
+    @resultado = Resultado.new(params[:resultado])
+    @resultado.reserva_id = params[:reserva_id]
+    @resultado.save!
+    redirect_to equipamento_reserva_path(params[:equipamento_id], params[:reserva_id])
+  end
 end
