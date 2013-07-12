8383838e95a70d6b63792b9b73dc0653af49eed4
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-07-11 23:02:35 -0300

    Mudando mensagem para nao autorizados

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
   	if usuario_signed_in?
-    	redirect_to root_path, :alert => "Você precisa logar como administrador para acessar essa página."
+    	redirect_to root_path, :alert => "Você não tem permissão para acessar essa página!"
     else
-    	redirect_to new_usuario_session_path, :alert => "Você precisa estar logado para acessar essa página."
+    	redirect_to new_usuario_session_path, :alert => "Você precisa estar logado para acessar essa página!"
     end
   end
 
   protect_from_forgery
   def current_ability
     @current_ability ||= Ability.new(current_usuario)
-end
+  end
 end
