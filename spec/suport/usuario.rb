77cef7a3d290d458f1674fbd0e31b550f49272c0
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-08-15 22:08:56 -0300

    Adicionando layout para login

--- spec/suport/usuario.rb
+++ spec/suport/usuario.rb
@@ -1,6 +1,7 @@
 def logar(usuario)
 	visit new_usuario_session_path
+  save_and_open_page
 	fill_in 'Email', with: usuario.email
 	fill_in 'Password', with: usuario.password
-	click_button 'Sign in'
+	click_button 'Entrar'
 end
