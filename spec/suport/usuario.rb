12121e7e875906b2abcf4a4d338bd5f93f5d4918
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-08-15 23:34:06 -0300

    Fixing Build

--- spec/suport/usuario.rb
+++ spec/suport/usuario.rb
@@ -1,7 +1,6 @@
 def logar(usuario)
-	visit new_usuario_session_path
-  save_and_open_page
-	fill_in 'Email', with: usuario.email
-	fill_in 'Password', with: usuario.password
-	click_button 'Entrar'
+  visit new_usuario_session_path
+  fill_in 'Email', with: usuario.email
+  fill_in 'Password', with: usuario.password
+  click_button 'Entrar'
 end
